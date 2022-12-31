178374a890aebd810d1d403e312ca409b837839c
liuzhaocheng <liuzhaocheng@kuaishou.com>
2022-12-31 22:19:27 +0800

    array.27

---
+++ array/27.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, j = 0;
+        while ( i < nums.size() ) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                ++i;
+                ++j;
+            } else {
+                while (i < nums.size() && nums[i] == val) {
+                    ++i;
+                }
+                if (i < nums.size()) {
+                    nums[j] = nums[i];
+                    ++i;
+                    ++j;
+                }
+            }
+        }
+        return j;
+    }
+};
